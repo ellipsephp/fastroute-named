f3f0b96e9008a30ca1b35630c7e2efd567ab8101
pmall <pierre.mallinjoud@gmail.com>
2018-02-08 17:42:23 +0100

    even better exception message

--- src/Exceptions/PlaceholderFormatException.php
+++ src/Exceptions/PlaceholderFormatException.php
@@ -8,7 +8,7 @@ class PlaceholderFormatException extends RuntimeException implements FastRouteEx
 {
     public function __construct(string $value, string $name, string $placeholder, string $format)
     {
-        $template = "The value '%s' does not match the format of the '%s' placeholder of the '%s' route ('%s')";
+        $template = "The value '%s' does not match the format expected by the '%s' placeholder of the '%s' route ('%s')";
 
         $msg = sprintf($template, $value, $placeholder, $name, $format);
 
